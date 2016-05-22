e35082f972fa15d3966657ee668b5b5592e1e6d8
isa_madrid90 <isa.madrid90@gmail.com>
2016-05-22 18:28:16 +0200

    Feature comments - Added form to create a comment, post data transformer used by the comment form and the comment form test

---
+++ src/Kodify/BlogBundle/Form/Type/CommentType.php
@@ -0,0 +1,37 @@
+<?php
+namespace Kodify\BlogBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text')
+            ->add('post', 'hidden')
+            ->add('author')
+            ->add('save', 'submit', ['attr' => ['class' => 'btn btn-success']]);
+        //add data transformer to use post as hidden type
+        if (isset($options['post_transformer'])) {
+            $builder->get('post')->addModelTransformer($options['post_transformer']);
+        }
+    }
+
+    public function getName()
+    {
+        return 'comment';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        //added option to pass post_transformer service
+        $resolver->setDefaults([
+            'post_transformer' => null
+        ]);
+    }
+
+
+}
