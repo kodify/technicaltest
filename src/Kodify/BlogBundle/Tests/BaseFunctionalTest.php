fd318e76da3bbec3e09936d1ae121dadfaeebe2a
isa_madrid90 <isa.madrid90@gmail.com>
2016-05-22 18:21:22 +0200

    Feature comments - Clear Comment table in cleanDb function

--- src/Kodify/BlogBundle/Tests/BaseFunctionalTest.php
+++ src/Kodify/BlogBundle/Tests/BaseFunctionalTest.php
@@ -4,7 +4,7 @@ namespace Kodify\BlogBundle\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class BaseFunctionalTest extends WebTestCase
+abstract class BaseFunctionalTest extends WebTestCase
 {
     protected $entityManager;
     protected $client;
@@ -24,6 +24,7 @@ class BaseFunctionalTest extends WebTestCase
     {
         $this->clearTableByName('Author');
         $this->clearTableByName('Post');
+        $this->clearTableByName('Comment');
     }
 
     protected function entityManager()
